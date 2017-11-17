5b576171762fb68bcc156b8ea29d75860c1b793f
Michelle Cinello <m.cinello@gmail.com>
2017-11-17 11:50:13 -0500

    created html response, but for some reason the response adds the entire html doc, and once you refresh the form is properly updated.

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,32 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  var form = document.querySelector('.new_tweet');
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      url: form.getAttribute('action'),
+      method: form.getAttribute('method'),
+      data: $(form).serialize(),
+      dataType: 'html'
+    })
+    .done(function(dataResponse) {
+      var tweetsList = document.querySelector('.tweets');
+      tweetsList.append(dataResponse);
+      console.log("hmtl resposne here");
+    });
+  });
+  console.log("html request sent");
+});
+
+//   var tweetsList = document.querySelector('.tweets');
+//   tweetsList.value = "";
+//
+//   var listItem = document.createElement('li');
+//   listeItem.className = 'tweet';
+//   listItem.innerHTML = dataResponse.message;
+// tweetsList.append(listItem);
